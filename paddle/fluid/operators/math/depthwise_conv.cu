783cb7a237929e6c9cdcd84698e5519017a3dad7
sabreshao <sabre.shao@amd.com>
2018-09-29 14:13:02 -0400

    [Don't merge!] Unknown asm code.

--- paddle/fluid/operators/math/depthwise_conv.cu
+++ paddle/fluid/operators/math/depthwise_conv.cu
@@ -37,13 +37,13 @@ __inline__ __device__ T warpReduceSum(T val) {
 }
 __forceinline__ __device__ unsigned lane_id() {
   unsigned ret;
-  asm volatile("mov.u32 %0, %laneid;" : "=r"(ret));
+  //asm volatile("mov.u32 %0, %laneid;" : "=r"(ret));
   return ret;
 }
 
 __forceinline__ __device__ unsigned warp_id() {
   unsigned ret;
-  asm volatile("mov.u32 %0, %warpid;" : "=r"(ret));
+  //asm volatile("mov.u32 %0, %warpid;" : "=r"(ret));
   return ret;
 }
 
